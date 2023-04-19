db15cd66c4146f6122cb7a51e9945c17ab934fb6
stivliz <stivliz0909@gmail.com>
2023-04-19 17:23:51 -0500

    fronted

--- PI-Videogames/api/src/routes/genres.route.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-const { getGenres } = require('../controllers/genres.controllers');
-const router = Router();
-
-router.get('/genres', getGenres)
-
-module.exports = router;
